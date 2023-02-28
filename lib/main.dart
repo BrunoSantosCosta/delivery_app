70f16bedd7df15330ab9fe9fc74ae318395e9c82
Bruno Santos <brunosanttos667@gmail.com>
2023-02-27 20:35:34 -0800

    Initial commit whit other itens build

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:food_app/auth/sign_in.dart';
+import 'package:food_app/screens/home_screen/home_screen.dart';
+
+void main() {
+  // await FirebaseAuth.instance;
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
